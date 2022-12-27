c956993a7aee10a9c4c9425749c64c3db3b1d2c6
JesusRamon2192 <JesusRamon2192@users.noreply.github.com>
2022-12-27 21:47:39 +0000

    deploy: d0d1e7f7f723c6dbfb94397e32c5777c61627679

---
+++ _dist_/lazy.js
@@ -0,0 +1,27 @@
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; // true dentro de la pantalla
+}
+
+const loadImage = (entry) => {
+    const container = entry.target; //container(DIV)
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    //Cargue imagen
+    imagen.src = url;
+    //console.log("holis");
+    //console.log(container.nodeName);
+    //desregistra la imagen (unlisten)
+    observer.unobserve(container);
+}
+
+const observer = new IntersectionObserver((entries) => {
+    //console.log(entries);
+    entries.filter(isIntersecting).forEach(loadImage);
+})
+
+//
+export const registerImage = (imagen) => {
+    //IntersectionObserver -> observer(imagen)
+    observer.observe(imagen);
+}
