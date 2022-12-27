d0d1e7f7f723c6dbfb94397e32c5777c61627679
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-27 15:46:58 -0600

    Proyecto implementando lazy loading para carga de imagenes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from "./lazy"
 
-console.log('Happy hacking :)')
+//Crear una imagen
+//Agregar #imagen
+
+const maximum = 122;
+const minimum = 1;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
+const createImageNode = () => {
+    const imagen = document.createElement("img")
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; //Todo
+
+    const container = document.createElement("div");
+    container.className = "p-4 bg-gray-300";
+    container.style.minHeight = "100px";
+    container.style.display = "inline-block";
+
+    container.appendChild(imagen);
+    
+    return container;
+};
+
+const mountNode = document.getElementById("images");
+const addButton = document.getElementById("add");
+const delButton = document.getElementById("del");
+
+//Add Images
+const addImages = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+    registerImage(newImage);
+    console.log(mountNode.innerHTML)
+}
+
+addButton.addEventListener("click", addImages)
+
+//Delete Images 
+const delImages = () => {
+    console.log(mountNode);
+    mountNode.innerHTML= ""
+}
+delButton.addEventListener("click", delImages)
+
