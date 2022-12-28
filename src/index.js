24fabbb4c1d7577313d872fb0360f9a624864f4d
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-28 16:13:31 -0600

    Reproductor de video terminado y se practica con hyperscript para declarar variables y crearlas en HTML desde JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {registerImage} from "./lazy"
+import h from "hyperscript"
 
 //Crear una imagen
 //Agregar #imagen
@@ -8,17 +9,25 @@ const minimum = 1;
 const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 const createImageNode = () => {
-    const imagen = document.createElement("img")
-    imagen.className = "mx-auto";
+    const imagen = h("img.mx-auto", {
+        width: "320",
+        "data-src": `https://randomfox.ca/images/${random()}.jpg`,
+    });
+    /* const imagen = document.createElement("img")
+    imagen.className = "mx-auto"; 
     imagen.width = "320";
-    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; //Todo
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; //Todo*/
 
-    const container = document.createElement("div");
-    container.className = "p-4 bg-gray-300";
-    container.style.minHeight = "100px";
+
+    const container = h("div.p-2.bg-gray-300", imagen, {
+        minHeight: "100px",
+    })
+    /* const container = document.createElement("div");
+    container.className = "p-4 bg-gray-300"; */
+    //container.style.minHeight = "100px";
     container.style.display = "inline-block";
 
-    container.appendChild(imagen);
+    //container.appendChild(imagen);
     appendedImages++;
     printLog();
     
